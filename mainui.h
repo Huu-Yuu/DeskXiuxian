807763b635df7bbfb61ef4e2a1fa42661424b85c
雨 胡 <1477328015@qq.com>
2023-08-01 23:00:24 +0800

    初始化提交

---
+++ mainui.h
@@ -0,0 +1,21 @@
+#ifndef MAINUI_H
+#define MAINUI_H
+
+#include <QMainWindow>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainUI; }
+QT_END_NAMESPACE
+
+class MainUI : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainUI(QWidget *parent = nullptr);
+    ~MainUI();
+
+private:
+    Ui::MainUI *ui;
+};
+#endif // MAINUI_H
