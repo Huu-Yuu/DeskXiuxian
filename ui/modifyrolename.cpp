827b211e54b6ca770e6e120c63694c30888bf934
yu.hu@saiterobot.com <yu.hu@saiterobot.com>
2023-08-25 18:09:08 +0800

    1、增加修改角色名界面 2、增加错误码，注册调试日志打印

--- ui/modifyrolename.cpp
+++ ui/modifyrolename.cpp
@@ -1,11 +1,25 @@
 #include "modifyrolename.h"
 #include "ui_modifyrolename.h"
 
-ModifyRoleName::ModifyRoleName(QWidget *parent) :
+ModifyRoleName::ModifyRoleName(QWidget* parent) :
     QDialog(parent),
     ui(new Ui::ModifyRoleName)
 {
     ui->setupUi(this);
+
+    // 设置logo
+    setWindowIcon(QIcon(":/res/logo.jpg"));
+
+    // 将窗口居中显示
+    setGeometry(
+        QStyle::alignedRect(
+            Qt::LeftToRight,
+            Qt::AlignCenter,
+            size(),
+            qApp->desktop()->availableGeometry()
+        )
+    );
+
 }
 
 ModifyRoleName::~ModifyRoleName()
