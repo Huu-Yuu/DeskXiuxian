819c6e4c499947a839bfe7fd526aaa97ae49c4ad
雨 胡 <1477328015@qq.com>
2023-09-10 19:01:01 +0800

    1、删除不用的信号和槽

--- DeskXiuxian.pro
+++ DeskXiuxian.pro
@@ -1,4 +1,4 @@
-QT       += core gui
+QT += core gui
 QT += sql
 QT += core
 QT += network
--- mainui.h
+++ mainui.h
@@ -140,26 +140,6 @@ public slots:
      */
     void SlotRenameSuccessful();
 
-//    /**
-//     * @brief UI初始化角色基本信息
-//     */
-//    void SlotRemoteRoleInfo(QJsonObject role_info_data);
-
-//    /**
-//     * @brief UI初始化角色装备信息
-//     */
-//    void SlotRemoteRoleEquip(QJsonObject role_info_data);
-
-//    /**
-//     * @brief UI初始化角色物品信息
-//     */
-//    void SlotRemoteRoleItem(QJsonObject role_info_data);
-
-//    /**
-//     * @brief UI初始化角色成长系数
-//     */
-//    void SlotRemoteRoleRC(QJsonObject role_info_data);
-
 protected:
     void closeEvent(QCloseEvent* event);
 
