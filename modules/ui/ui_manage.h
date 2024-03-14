8920b96a7f18242c5c1fb5cd1b487a0c94ec4f57
yu.hu@saiterobot.com <yu.hu@saiterobot.com>
2024-03-14 17:41:01 +0800

    重构代码：连接主要信号槽

---
+++ modules/ui/ui_manage.h
@@ -0,0 +1,23 @@
+#ifndef DESKXIUXINA_UI_MANAGE_H
+#define DESKXIUXINA_UI_MANAGE_H
+#include <QObject>
+#include "modules/interface_manager.h"
+#include "mainui.h"
+
+class UIManage : public InterfaceManager
+{
+Q_OBJECT
+public:
+    UIManage();
+    ~UIManage();
+    int Init() override;
+
+public slots:
+    void SlotActionResponse(const QJsonObject& request_data) override;
+    void SlotActionRequest(const QJsonObject& request_data) override;
+    void SlotPubTopic(const QJsonObject& status) override;
+
+private:
+    MainUI* ui_obj_;
+};
+#endif //DESKXIUXINA_UI_MANAGE_H
